e5a199919f2ec894beba8ac08d4b4e6575ab9c9a
Hamed-Fouladi <hamed.fouladi27@gmail.com>
2020-08-23 17:19:15 +0300

    Footer

--- header.php
+++ header.php
@@ -20,7 +20,9 @@
 
             </div>
             <div class="header_third logo_section">
-                <a href="<?php echo home_url('/'); ?>" class="logo_link"><img src="<?php echo ale_get_option('sitelogo')?>"/></a>
+                <?php if(ale_get_option('sitelogo')){ ?>
+                    <a href="<?php echo home_url('/'); ?>" class="logo_link"><img src="<?php echo ale_get_option('sitelogo')?>"/></a>
+                <?php } ?>
             </div>
             <div class="header_third social_section">
                 <?php if (ale_get_option('yt')) {?><a href="<?php echo ale_get_option('yt') ?>"><i class="fab fa-youtube"></i></a><?php } ?>
